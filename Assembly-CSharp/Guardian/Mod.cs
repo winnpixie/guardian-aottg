216a28d84b6d075788efd6b4fca8393d5930b51c
Summer <salvadin2@gmail.com>
2021-09-12 05:02:56 -0400

    woops

--- Assembly-CSharp/Guardian/Mod.cs
+++ Assembly-CSharp/Guardian/Mod.cs
@@ -14,7 +14,7 @@ namespace Guardian
 {
     class Mod : MonoBehaviour
     {
-        public static string Build = "09122021";
+        public static string Build = "09122021-1";
         public static string RootDir = Application.dataPath + "\\..";
         public static string HostWhitelistPath = RootDir + "\\Hosts.txt";
 
--- Assembly-CSharp/Guardian/Utilities/ResourceHelper.cs
+++ Assembly-CSharp/Guardian/Utilities/ResourceHelper.cs
@@ -11,33 +11,38 @@ namespace Guardian.Utilities
         {
             if (!s_resourceCache.TryGetValue(path, out object res))
             {
-                using WWW www = new WWW("file:///" + Application.streamingAssetsPath + $"/{path}");
-                while (!www.isDone) { }
+                    using WWW www = new WWW("file:///" + Application.streamingAssetsPath + $"/{path}");
+                    while (!www.isDone) { }
 
-                System.Type type = typeof(T);
-                if (typeof(MovieTexture).IsAssignableFrom(type))
-                {
-                    res = www.movie;
-                }
-                else if (typeof(Texture).IsAssignableFrom(type))
-                {
-                    res = www.texture;
-                }
-                else if (typeof(AudioClip).IsAssignableFrom(type))
-                {
-                    res = www.audioClip;
-                }
-                else if (typeof(string).IsAssignableFrom(type))
-                {
-                    res = www.text;
-                }
-                else
-                {
-                    // When in doubt, return raw bytes
-                    res = www.bytes;
-                }
+                    if (www.error != null)
+                    {
+                        return default(T);
+                    }
 
-                s_resourceCache.Add(path, res);
+                    System.Type type = typeof(T);
+                    if (typeof(MovieTexture).IsAssignableFrom(type))
+                    {
+                        res = www.movie;
+                    }
+                    else if (typeof(Texture).IsAssignableFrom(type))
+                    {
+                        res = www.texture;
+                    }
+                    else if (typeof(AudioClip).IsAssignableFrom(type))
+                    {
+                        res = www.audioClip;
+                    }
+                    else if (typeof(string).IsAssignableFrom(type))
+                    {
+                        res = www.text;
+                    }
+                    else
+                    {
+                        // When in doubt, return raw bytes
+                        res = www.bytes;
+                    }
+
+                    s_resourceCache.Add(path, res);
             }
 
             return (T)res;
